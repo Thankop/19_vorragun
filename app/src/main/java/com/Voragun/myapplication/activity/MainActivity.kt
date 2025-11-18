298117f0506fd4110763a6008bbb9930fdaaa834
VorragunVerapongpradit <thank.vorragun@gmail.com>
2025-11-18 09:22:04 +0700

    first commit

---
+++ app/src/main/java/com/Voragun/myapplication/activity/MainActivity.kt
@@ -0,0 +1,79 @@
+  package com.Voragun.myapplication.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.Voragun.myapplication.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+  class MainActivity : AppCompatActivity() {
+
+      //สร้างตัวแปรไว้สำหรับทำงานกับ view Binging
+      lateinit var navController: NavController
+      lateinit var appBarConfiguration: AppBarConfiguration
+      lateinit var navHostFragment: NavHostFragment
+      lateinit var mainToolbar: Toolbar
+      lateinit var mainDrawerLayout: DrawerLayout
+      lateinit var mainNavigationView: NavigationView
+      lateinit var mainBottomNavigationView: BottomNavigationView
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //ทำการ Find View
+        mainToolbar = findViewById(R.id.main_toolbar)
+        mainDrawerLayout = findViewById(R.id.drawer_layout)
+        mainNavigationView = findViewById(R.id.main_navigation_view)
+        mainBottomNavigationView = findViewById(R.id.main_bottom_navigation_view)
+
+        //กำหนดค่าเริ่มต้นให้กับ
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nave_host) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //กำหนด appBar
+        appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.homeFragment,
+            R.id.productFragment,
+            R.id.addproductFragment,
+            R.id.notificationFragment,
+            R.id.accountFragment
+        ).setOpenableLayout(mainDrawerLayout).build()
+
+        //เรียกการทำงานกัน Toolbar
+        setSupportActionBar(mainToolbar)
+
+        //toolbar แสดง Icon เมนู
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        //เรียกใช้งานเมนูด้านข้าง
+        mainNavigationView.setupWithNavController(navController)
+
+        //เรียกใช้เมนูด้านล่าง
+        mainBottomNavigationView.setupWithNavController(navController)
+
+    }
+      override fun onSupportNavigateUp(): Boolean {
+          return NavigationUI.navigateUp(navController,appBarConfiguration)
+      }
+}
